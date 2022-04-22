c62799556abd586d06786dbdad0b42c6ebc5d307
Nickspizza001 <dbodun56@gmail.com>
2022-04-22 04:29:48 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors);
 app.use(morgan('combined'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, ()=>{
     console.log(`app started on port ${PORT}`);
 });
